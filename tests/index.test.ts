7d27d93d5f8787bb2e01b2818baf5a8b4fba0530
Peter Paul Kiefer <dafisppk@gmail.com>
2024-02-03 15:47:20 +0100

    T: first structural tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,33 @@
-describe('testing index file', () => {
-  test('bla', () => {
-    expect(true).toBe(true);
-  })
-})
+// import from index file ensures that all tested 
+// artifacts are exported. The test only have to make sure that
+// the objects are instanciable
+import {TableBuilder, GridArea} from '../src/constable';
+
+
+describe('Structural Tests', () => {
+  // o_u_t :=: object under test
+  test('TableBuilder is instanciable', () => {
+    const o_u_t = new TableBuilder("");
+    expect(o_u_t).toBeInstanceOf(TableBuilder);
+  });
+  test('GridArea is instanciable', () => {
+    const o_u_t = new GridArea("");
+    expect(o_u_t).toBeInstanceOf(GridArea);
+  });
+});
+
+describe('key fields work as expected', () => {
+  // o_u_t means object under test
+  // TEST_KEY is used for id or name
+  const TEST_KEY = "test1";
+  test('TableBuilder has valid name', () => {
+    const o_u_t = new TableBuilder(TEST_KEY);
+    expect(o_u_t).toBeInstanceOf(TableBuilder);
+    expect(o_u_t.name).toBe(TEST_KEY)
+  });
+  test('GridArea has valid id', () => {
+    const o_u_t = new GridArea(TEST_KEY);
+    expect(o_u_t).toBeInstanceOf(GridArea);
+    expect(o_u_t.id).toBe(TEST_KEY)
+  });
+});
