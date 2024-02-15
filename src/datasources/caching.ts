cb31ef3ed8e37451e8c5b135839a1271f6a49675
Peter Paul Kiefer <dafisppk@gmail.com>
2024-02-15 16:35:15 +0100

    an idea for data schemas
    I will change this again. I need to implement the table
    elements like building blocks, dataschemas/datasources
    and renderer at first. Then I'll implement composer functions
    to assemble the bricks to higher components.

--- src/datasources/caching.ts
+++ src/datasources/caching.ts
@@ -1,32 +1,9 @@
-/**
- * @class
- */
-class AbstractDataCache {
+class DataCache {
   id: string;
   size: number
   constructor(id: string, size: number) {
     this.id = id;
     this.size = size;
-  }
-}
-
-/**
- * @class
- */
-class SimpleDataCache extends AbstractDataCache {
-  constructor(id: string, size: number) {
-    super(id, size);
-  }
-}
-
-/**
- * @class
- */
-class WebStorageDataCach extends AbstractDataCache {
-  storageName: string;
-  constructor(id: string, size: number, storageName: string) {
-    super(id, size);
-    this.storageName = storageName;
   }
 }
 
--- src/datasources/dataschema.ts
+++ src/datasources/dataschema.ts
@@ -2,46 +2,117 @@ type FieldDataType = string | number | Date | boolean | {};
 
 class FieldBase {
   _name: string;
-  constructor(name: string) {
+  _typeString: string;
+  constructor(name: string, typeString: string) {
     this._name = name;
+    this._typeString = typeString;
   }
-
   get name(): string {
     return this._name;
   }
 }
 
 interface Field<T extends FieldDataType> {
-  extractValue(record: { [key: string]: FieldDataType }): T | undefined;
+  extractValue(record: { [key: string]: FieldDataType }): T;
   get name(): string;
 }
 
 class TextField extends FieldBase implements Field<string> {
-  value: string;
-
-  constructor(name: string, value: string) {
-    super(name);
-    this.value = value;
+  constructor(name: string) {
+    super(name, "text");
   }
-
-  extractValue(record: { [key: string]: FieldDataType }): string | undefined {
+  extractValue(record: { [key: string]: FieldDataType }): string {
     const value = record[this.name];
     if (typeof value === "string") {
       return value;
     }
     // TODO: implement converter
-    return undefined;
+    throw Error(
+      `type of record element ${this.name} (type = ${typeof value}) is not a string.`,
+    );
   }
 }
 
-/**
- * @class
- */
-class DataRecord {
-  name: string;
-  fields: [Field<any>?];
+class NumberField extends FieldBase implements Field<number> {
   constructor(name: string) {
-    this.name = name;
-    this.fields = [];
+    super(name, "number");
+  }
+  extractValue(record: { [key: string]: FieldDataType }): number {
+    const value = record[this.name];
+    if (typeof value === "number") {
+      return value;
+    }
+    // TODO: implement converter
+    throw Error(
+      `type of record element ${this.name} (type = ${typeof value}) is not a number.`,
+    );
   }
 }
+
+class BooleanField extends FieldBase implements Field<boolean> {
+  constructor(name: string) {
+    super(name, "bool");
+  }
+  extractValue(record: { [key: string]: FieldDataType }): boolean {
+    const value = record[this.name];
+    if (typeof value === "boolean") {
+      return value;
+    }
+    // TODO: implement converter
+    throw Error(
+      `type of record element ${this.name} (type = ${typeof value}) is not a boolean.`,
+    );
+  }
+}
+
+class DateField extends FieldBase implements Field<Date> {
+  constructor(name: string) {
+    super(name, "date");
+  }
+  extractValue(record: { [key: string]: FieldDataType }): Date {
+    const value = record[this.name];
+    if (value instanceof Date) {
+      return value;
+    }
+    // TODO: implement converter
+    throw Error(
+      `type of record element ${this.name} (type = ${typeof value}) is not a boolean.`,
+    );
+  }
+}
+
+class DataRecord {
+  name: string;
+  fields: { [name: string]: Field<FieldDataType> };
+
+  constructor(name: string) {
+    this.name = name;
+    this.fields = {};
+  }
+
+  addTextField(name: string): TextField {
+    const field = new TextField(name);
+    this.fields[name] = field;
+    return field;
+  }
+
+  addNumberField(name: string): NumberField {
+    const field = new NumberField(name);
+    this.fields[name] = field;
+    return field;
+  }
+
+  addBooleanField(name: string): BooleanField {
+    const field = new BooleanField(name);
+    this.fields[name] = field;
+    return field;
+  }
+
+  addDateField(name: string): DateField {
+    const field = new DateField(name);
+    this.fields[name] = field;
+    return field;
+  }
+}
+
+export { DataRecord };
